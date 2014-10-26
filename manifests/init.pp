2bc32bb496b9d8f7df43012c26a902cc2074ccf4
Jeremy T. Bouse <Jeremy.Bouse@UnderGrid.net>
2014-10-26 02:13:14 -0400

    Work on main.inc.php template and spec tests

--- manifests/init.pp
+++ manifests/init.pp
@@ -8,20 +8,51 @@
 #   Explanation of what this parameter affects and what it defaults to.
 #
 class roundcube (
-  $confdir           = $::roundcube::params::confdir,
-  $backend           = $::roundcube::params::backend,
-  $database_host     = $::roundcube::params::database_host,
-  $database_port     = $::roundcube::params::database_port,
-  $database_name     = $::roundcube::params::database_name,
-  $database_username = $::roundcube::params::database_username,
-  $database_password = $::roundcube::params::database_password,
-  $database_ssl      = $::roundcube::params::database_ssl,
-  $main_inc_php_erb  = $::roundcube::params::main_inc_php_erb,
+  $confdir             = $::roundcube::params::confdir,
+  $backend             = $::roundcube::params::backend,
+  $database_host       = $::roundcube::params::database_host,
+  $database_port       = $::roundcube::params::database_port,
+  $database_name       = $::roundcube::params::database_name,
+  $database_username   = $::roundcube::params::database_username,
+  $database_password   = $::roundcube::params::database_password,
+  $database_ssl        = $::roundcube::params::database_ssl,
+  $main_inc_php_erb    = $::roundcube::params::main_inc_php_erb,
+  $log_logins          = undef,
+  $default_host        = undef,
+  $default_port        = undef,
+  $imap_auth_type      = undef,
+  $smtp_server         = undef,
+  $smtp_port           = undef,
+  $smtp_user           = undef,
+  $smtp_pass           = undef,
+  $smtp_auth_type      = undef,
+  $support_url         = undef,
+  $force_https         = undef,
+  $session_domain      = undef,
+  $ip_check            = undef,
+  $referer_check       = undef,
+  $des_key             = undef,
+  $username_domain     = undef,
+  $mail_domain         = undef,
+  $product_name        = undef,
+  $include_host_config = undef,
+  $plugins             = undef,
+  $skin                = undef,
+  $timezone            = undef,
+  $default_font        = undef,
 ) inherits roundcube::params {
 
   validate_re($backend, '^(mysql|pgsql|sqlite3)$')
   validate_bool($database_ssl)
   validate_absolute_path($confdir)
+
+  if $include_host_config {
+    validate_hash($include_host_config)
+  }
+
+  if $plugins {
+    validate_array($plugins)
+  }
 
   class { 'roundcube::install': } ->
   class { 'roundcube::config': } ->
--- spec/classes/roundcube_spec.rb
+++ spec/classes/roundcube_spec.rb
@@ -107,10 +107,105 @@ describe 'roundcube' do
             'mode'  => '0640',
           }) }
 
+      it { should contain_file('/etc/roundcube/main.inc.php')
+        .with_content %r{^\$rcmail_config\['include_host_config'\] = false;$} }
+
+      it { should contain_file('/etc/roundcube/main.inc.php')
+        .with_content %r{^\$rcmail_config\['plugins'\] = array\(\);$} }
+
       it { should contain_package('roundcube').with_ensure('present') }
       it { should contain_package('roundcube-core').with_ensure('present') }
       it { should contain_package('roundcube-plugins').with_ensure('present') }
       it { should contain_package("roundcube-pgsql").with_ensure('present') }
+    end
+
+    describe "roundcube class with custom parameters" do
+      let(:params) {{
+        :database_name       => 'roundcubedb',
+        :database_username   => 'roundcubedb',
+        :database_password   => 'roundcubedb',
+        :default_host        => 'ssl://imap.example.com',
+        :smtp_server         => 'tls://smtp.example.com',
+        :smtp_port           => '587',
+        :session_domain      => '.example.com',
+        :username_domain     => '%z',
+        :mail_domain         => '%z',
+        :force_https         => true,
+        :include_host_config => {
+          'example.com' => 'example.com.inc.php',
+          'example.org' => 'example.org.inc.php',
+        },
+        :plugins             => [
+          'archive',
+          'markasjunk',
+        ],
+      }}
+
+      it { should compile.with_all_deps }
+
+      it { should contain_ini_setting('dbuser').with({
+            'ensure'  => 'present',
+            'path'    => '/etc/dbconfig-common/roundcube.conf',
+            'section' => '',
+            'notify'  => 'Exec[reconfigure-roundcube]',
+            'setting' => 'dbc_dbuser',
+            'value'   => "'roundcubedb'",
+            'require' => "Package[roundcube-pgsql]",
+          }) }
+
+      it { should contain_ini_setting('dbpass').with({
+            'ensure'  => 'present',
+            'path'    => '/etc/dbconfig-common/roundcube.conf',
+            'section' => '',
+            'notify'  => 'Exec[reconfigure-roundcube]',
+            'setting' => 'dbc_dbpass',
+            'value'   => "'roundcubedb'",
+            'require' => "Package[roundcube-pgsql]",
+          }) }
+
+      it { should contain_ini_setting('dbname').with({
+            'ensure'  => 'present',
+            'path'    => '/etc/dbconfig-common/roundcube.conf',
+            'section' => '',
+            'notify'  => 'Exec[reconfigure-roundcube]',
+            'setting' => 'dbc_dbname',
+            'value'   => "'roundcubedb'",
+            'require' => "Package[roundcube-pgsql]",
+          }) }
+
+      it { should contain_file('/etc/roundcube/main.inc.php')
+        .with_content %r{^\$rcmail_config\['default_host'\] = 'ssl://imap.example.com';$} }
+
+      it { should contain_file('/etc/roundcube/main.inc.php')
+        .with_content %r{^\$rcmail_config\['smtp_server'\] = 'tls://smtp.example.com';$} }
+
+      it { should contain_file('/etc/roundcube/main.inc.php')
+        .with_content %r{^\$rcmail_config\['smtp_port'\] = 587;$} }
+
+      it { should contain_file('/etc/roundcube/main.inc.php')
+        .with_content %r{^\$rcmail_config\['session_domain'\] = '.example.com';$} }
+
+      it { should contain_file('/etc/roundcube/main.inc.php')
+        .with_content %r{^\$rcmail_config\['username_domain'\] = '%z';$} }
+
+      it { should contain_file('/etc/roundcube/main.inc.php')
+        .with_content %r{^\$rcmail_config\['mail_domain'\] = '%z';$} }
+
+      it { should contain_file('/etc/roundcube/main.inc.php')
+        .with_content %r{^\$rcmail_config\['force_https'\] = true;$} }
+
+      it { should contain_file('/etc/roundcube/main.inc.php')
+        .without_content %r{^\$rcmail_config\['include_host_config'\] = false;$} }
+
+      it { should contain_file('/etc/roundcube/main.inc.php')
+        .with_content %r{^\$rcmail_config\['include_host_config'\] = array\(\n\s*'example.com' => 'example.com.inc.php',\n\s*'example.org' => 'example.org.inc.php',\n\);$} }
+
+      it { should contain_file('/etc/roundcube/main.inc.php')
+        .without_content %r{^\$rcmail_config\['plugins'\] = array\(\);$} }
+
+      it { should contain_file('/etc/roundcube/main.inc.php')
+        .with_content %r{^\$rcmail_config\['plugins'\] = array\('archive','markasjunk'\);$} }
+
     end
   end
 end
