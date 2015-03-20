fe65915b2eec74cd41251e0b665baed402627cda
Luke Bigum <Luke.Bigum@lmax.com>
2015-03-20 12:29:10 +0000

    placeholder class file roundcube::db::mysql

---
+++ manifests/db/mysql.pp
@@ -0,0 +1,11 @@
+# == Class: roundcube::db::mysql
+#
+# Placeholder class to create a MySQL database for RoundCube.
+#
+# === Parameters
+#
+#   None
+#
+class roundcube::db::mysql {
+  #LB: not implementing yet
+}
